ff24047dfc29747a3832c0e31c648ec5dc9f198b
Malgorzata Kaczmarczyk-Koza <kaczmarczyk.mmm@wp.pl>
2023-01-31 12:48:20 +0100

    Add remove from cart function

--- src/js/script.js
+++ src/js/script.js
@@ -433,6 +433,10 @@
 
       thisCart.dom.productList.addEventListener('update', function(){
         thisCart.update();
+      });
+
+      thisCart.dom.productList.addEventListener('remove', function(){
+        thisCart.remove(event.detail.cartProduct);
       })
 
     }
@@ -469,6 +473,9 @@
 
       if (thisCart.totalNumber !== 0) {
         thisCart.totalPrice = thisCart.subtotalPrice + deliveryFee;
+      } else {
+        thisCart.totalPrice = 0;
+        thisCart.totalNumber = 0;
       }
 
       console.log('thisCart.totalNumber',thisCart.totalNumber);
@@ -481,6 +488,18 @@
         totalPrice.innerHTML = thisCart.totalPrice;
       }
       thisCart.dom.totalNumber.innerHTML = thisCart.totalNumber;
+
+    }
+
+    remove(cartProduct){
+      const thisCart = this;
+
+      cartProduct.dom.wrapper.remove();
+
+      const index = thisCart.products.indexOf(cartProduct);
+      thisCart.products.splice(index, 1);
+
+      thisCart.update();
 
     }
   }
@@ -500,6 +519,7 @@
 
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
 
     }
 
@@ -527,6 +547,35 @@
         thisCartProduct.price = thisCartProduct.amount * thisCartProduct.priceSingle;
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
       });  
+
+    }
+
+    remove(){
+      const thisCartProduct = this;
+
+      console.log('remove');
+
+      const event = new CustomEvent('remove', {
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+    }
+
+    initActions() {
+      const thisCartProduct = this;
+
+      thisCartProduct.dom.edit.addEventListener('click', function(event) {
+        event.preventDefault();
+      });
+
+      thisCartProduct.dom.remove.addEventListener('click', function(event) {
+        event.preventDefault();
+        thisCartProduct.remove();
+      });
 
     }
     
